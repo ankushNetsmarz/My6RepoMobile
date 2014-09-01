9ff3771ea7cc6b371691b7eadc39f8705611fb1d
ankush19902@gmail.com <weexcel@Weexcels-Mac-mini-2.local>
2014-09-01 19:19:20 +0530

    initial Commit

---
+++ my6/platforms/ios/www/js/disconnect.js
@@ -0,0 +1,29 @@
+﻿var userId = localStorage.getItem("userId");
+
+var userProfileid = localStorage.getItem("profileId");
+
+function disConnect() {
+
+    var inputdata = {
+        "UserID": userId,
+       "friendUserID": userProfileid
+    };
+
+    $.ajax({
+        type: "POST",
+    //   url: "http://localhost:4103/api/User/disConnectUser",
+        url: "http://174.141.233.6/MY6/api/User/disConnectUser",
+        data: inputdata,
+        success: function (data) {
+           // console.log(data);
+           
+           window.plugins.toast.show('Disconnected !', 'short', 'center', function(a){}, function(b){});
+           
+           window.location.replace("myfriends.html");
+         
+        },
+        error: function (xhr) {
+            alert(xhr.responseText);
+        }
+    });
+}
---
+++ my6/platforms/ios/www/js/my6.js
@@ -0,0 +1,406 @@
+var namesInterest = [];
+var interestResponseNew = [];
+var IdInterest = [];
+var interestResponseIdNew = [];
+var urlPic;
+var ResponseArray = [];
+var user = {};
+
+//ADD or CREATE
+function AddNewUser() {
+    var faceBookUrl = localStorage.getItem("facebookUrl");
+
+
+    var userData = {
+        "email": $("#emails").val(),
+        "password": $("#UserPassword").val(),
+        "firstName": $("#Firsti").val(),
+        "lastName": $("#Lasts").val(),
+        "isRegisteredByFacebook": true,
+        "FacebookPicURL": faceBookUrl
+
+    };
+
+    if(navigator.connection.type==Connection.WIFI || navigator.connection.type==Connection.CELL_3G )
+    {
+
+    $.ajax({
+        type: "POST",
+        beforeSend: showLoader(),
+        url: "http://174.141.233.6/MY6/api/user/AddUser",
+        data: userData,
+
+        success: function (data) {
+            console.log(data);
+            var userId = data.ResponseData;
+            localStorage.setItem("userId", userId);
+
+            var sucess = data.IsSucess;
+
+            if (sucess == "false") {
+              //  $(".message-area").text("please try again, may be already registered");
+              //  $(".alert-box").show();
+window.plugins.toast.show('User already registered !', 'short', 'center', function(a){}, function(b){});
+            }
+            else {
+                window.localStorage.setItem("login", "submit");
+                window.location.replace("interest.html");
+            }
+        },
+        error: function (xhr) {
+          //  $(".message-area").text(xhr.responseText);
+          //  $(".alert-box").show();
+           hideLoader();
+           window.plugins.toast.show('Some error occured !', 'short', 'center', function(a){}, function(b){});
+
+        }
+    }).done(function () {
+        hideLoader();
+    });
+    }
+    else
+    {
+       window.plugins.toast.show('Internet not available!', 'short', 'center', function(a){}, function(b){});
+    }
+}
+
+//update user
+function UpdateUser() { 
+    var editNameNew = $("#EditName").val().split(" ");
+    var gender = localStorage.getItem("gender");
+    if (gender == null) {
+       
+        gender = "Male";
+    }
+    var userData =
+        {
+            "userID": localStorage.getItem("userId"),
+            "DOB": $("#EditDOB").val(),
+            "firstName": editNameNew[0],
+            "lastName": editNameNew[1],
+            "contactNo": $("#EditContact").val(),
+            "Gender": gender,
+            "locationAddress": $("#EditLocation").val()
+        };
+    console.log(userData);
+    //alert(userData);
+    $.ajax({
+        type: "POST",
+        beforeSend: showLoader(),
+        url: "http://174.141.233.6/MY6/api/user/UpdateUser",
+        data: userData,
+        success: function (data) {
+         
+           
+window.plugins.toast.show('Profile Updated !', 'long', 'center', function(a){}, function(b){});
+            GetUserData();
+        },
+        error: function (xhr) {
+         //   alert(xhr.responseText);
+        }
+    }).done(function () {
+        hideLoader();
+    });
+}
+
+function GetUserData() {
+
+    var inputdata = {
+        "UserID": localStorage.getItem("userId")
+    };
+
+    $.ajax({
+        type: "GET",
+        beforeSend: showLoader(),
+        //url: "http://localhost:4103/api/User/GetUserData",
+        url: "http://174.141.233.6/MY6/api/User/GetUserData",
+        data: inputdata,
+        success: function (data) {
+            console.log(data);
+            
+            var profilePic= data.ResponseData[0].ProfilePicName;
+         
+       
+            if (profilePic == null || profilePic == "") {
+
+            	profilePic = "img/white-logo-2.png";
+              
+            }
+
+            else {
+
+            	profilePic = "http://174.141.233.6/my6/Files/ProfilePictures/" + data.ResponseData[0].ProfilePicName;
+              
+            }
+        
+           var Gender = data.ResponseData[0].Gender;
+           var DOB = data.ResponseData[0].DOB;
+           var LocationAddress = data.ResponseData[0].LocationAddress;
+           var status = data.ResponseData[0].Status;
+           var ContactNo= data.ResponseData[0].ContactNo;
+            if (Gender == null) {
+                Gender = "Not Available";
+            }
+            if (DOB == null) {
+                DOB = "";
+            }
+            if (LocationAddress == null) {
+                LocationAddress = "Not Available";
+            }
+            if (status == null) {
+                status = "no status";
+            }
+          
+            if (ContactNo == null) {
+                ContactNo = "Not Available";
+            }
+            localStorage.setItem("ProfilePic",profilePic);
+            //    alert(data.ResponseData[0].interestsID);
+            user.rating = data.ResponseData[0].UserRating;
+            user.id = data.ResponseData[0].UserID;
+            user.FirstName = data.ResponseData[0].FirstName;
+            user.LastName = data.ResponseData[0].LastName;
+            user.DOB = DOB;
+            user.ContactNo = ContactNo;
+            user.LocationAddress = LocationAddress;
+            user.Gender = Gender;
+            user.interests = data.ResponseData[0].interests;
+            user.status = status;
+           
+            
+            var interestResponse = data.ResponseData[0].interests;
+            var interestResponseId = data.ResponseData[0].interestsID;
+         
+
+            interestResponseNew = interestResponse.split("|");
+            interestResponseIdNew = interestResponseId.split("|");
+          
+            
+
+            for (var i = 0; i < 6; i++) {
+                namesInterest.push(interestResponseNew[i]);
+                IdInterest.push(interestResponseIdNew[i]);
+            }
+
+            localStorage.setItem("interestResponse", JSON.stringify(namesInterest));
+            localStorage.setItem("interestResponseId", JSON.stringify(IdInterest));
+
+
+            
+            localStorage.setItem("userId", data.ResponseData[0].UserID);
+            localStorage.setItem("ResponseArray", JSON.stringify(user));
+
+
+
+        },
+        error: function (xhr) {
+          //  alert(xhr.responseText);
+        }
+    }).done(function () {
+        hideLoader();
+    });
+}
+
+
+
+//Add interests
+function AddInterests() {
+    var userIdConnect = localStorage.getItem("userId");
+    var Interests = JSON.parse(localStorage.getItem("newId"));
+
+
+    var inputdata = {
+
+        "UserID": userIdConnect,
+        "Interests": Interests + ","
+    };
+
+    $.ajax({
+        type: "GET",
+        // url: "http://localhost:4103/api/user/AddInterest/",
+        url: "http://174.141.233.6/MY6/api/user/AddInterest/",
+        data: inputdata,
+        success: function (data) {
+           // console.log(data);
+           // toastr.success("interest added");
+           
+window.plugins.toast.show('Interest Added !', 'short', 'center', function(a){}, function(b){});
+        },
+        error: function (xhr) {
+//            $(".message-area").text(xhr.responseText);
+//            $(".alert-box").show();
+window.plugins.toast.show('Some error occured, Please try again !', 'short', 'center', function(a){}, function(b){});
+        }
+    });
+}
+
+
+function IsEmailExists() {
+    var userData = {
+        "email": $("#emails").val()
+    };
+
+    $.ajax({
+        type: "GET",
+        beforeSend: showLoader(),
+        url: "http://174.141.233.6/MY6/api/user/IsEmailExists",
+        data: userData,
+        success: function (data) {
+            console.log(data);
+            var res = data.ResponseData;
+            if (res == 1) {
+//                $(".message-area").text("user is already registered");
+//                $(".alert-box").show();
+window.plugins.toast.show('User is already registered !', 'long', 'center', function(a){}, function(b){});
+                $("#emails").val('');
+                $("#emails").focus();
+            }
+
+        },
+        error: function (xhr) {
+//            $(".message-area").text(xhr.responseText);
+//            $(".alert-box").show();
+
+           window.plugins.toast.show('Some error occured, Please try again !', 'long', 'center', function(a){}, function(b){});
+
+        }
+    }).done(function () {
+        hideLoader();
+    });
+}
+
+//Validate User
+function validateUser() {
+
+    var inputdata = {
+        "email": $("#emailLogin").val(),
+        "password": $("#passwordLogin").val()
+    };
+    if(navigator.connection.type==Connection.WIFI || navigator.connection.type==Connection.CELL_3G )
+    {
+        
+    
+    $.ajax({
+        type: "GET",
+        beforeSend: showLoader(),
+        url: "http://174.141.233.6/MY6/api/user/GetLogin/",
+        data: inputdata,
+        success: function (data) {
+        	if(data.ResponseData[0].UserID != 0)
+        		{
+        	    createDatabase();
+                        
+        	    var Gender = data.ResponseData[0].Gender;
+        	    var DOB = data.ResponseData[0].DOB;
+        	    var LocationAddress = data.ResponseData[0].LocationAddress;
+        	    var status = data.ResponseData[0].Status;
+        	    var ContactNo = data.ResponseData[0].ContactNo;
+        	    if (Gender == null) {
+        	        Gender = "Not Available";
+        	    }
+        	    if (DOB == null) {
+        	        DOB = "Not Available";
+        	    }
+        	    if (LocationAddress == null) {
+        	        LocationAddress = "Not Available";
+        	    }
+        	    if (status == null) {
+        	        status = "no status";
+        	    }
+        	    if (ContactNo == null) {
+        	        ContactNo = "Not Available";
+        	    }
+        	  user.rating = data.ResponseData[0].UserRating;
+            user.id = data.ResponseData[0].UserID;
+            user.FirstName = data.ResponseData[0].FirstName;
+            user.LastName = data.ResponseData[0].LastName;
+            user.DOB = DOB;
+            user.ContactNo = ContactNo;
+            user.LocationAddress = LocationAddress;
+            user.Gender = Gender;
+            user.interests = data.ResponseData[0].interests;
+            user.status = status;
+            localStorage.setItem("userId", data.ResponseData[0].UserID);
+
+            localStorage.setItem("ResponseArray", JSON.stringify(user));
+           localStorage.setItem("emailID",data.ResponseData[0].Email);
+            var respons = data.ResponseData[0].UserID;
+
+            var interestResponse = data.ResponseData[0].interests;
+            var interestResponseId = data.ResponseData[0].interestsID;
+
+            var facebookUrl = data.ResponseData[0].FacebookProfilePicUrl;
+            var ProfilePicUrl = data.ResponseData[0].ProfilePicName;
+
+            if (ProfilePicUrl != null) {
+                urlPic = "http://174.141.233.6/my6/Files/ProfilePictures/" + ProfilePicUrl;
+            }
+         //   else if (facebookUrl != null) {
+               // urlPic = facebookUrl;
+         //   }
+            else {
+                urlPic = "img/white-logo-2.png";
+            }
+
+            localStorage.setItem("ProfilePic", urlPic);
+
+     
+            interestResponseNew = interestResponse.split("|");
+            interestResponseIdNew = interestResponseId.split("|");
+
+            for (i = 0; i < 6; i++) {
+                namesInterest.push(interestResponseNew[i]);
+                IdInterest.push(interestResponseIdNew[i]);
+            }
+
+            localStorage.setItem("interestResponse", JSON.stringify(namesInterest));
+            localStorage.setItem("interestResponseId", JSON.stringify(IdInterest));
+
+
+
+            if (respons == "0") {
+//                $(".message-area").text("please try again");
+//                $(".alert-box").show();
+           window.plugins.toast.show('Please try again !', 'long', 'center', function(a){}, function(b){});
+
+            }
+            else {
+                window.location.replace("interest.html");
+            }
+        		}
+        	else
+//        		   $(".message-area").text("Please try again!!");
+//            $(".alert-box").show();
+      
+           window.plugins.toast.show('Please try again !', 'long', 'center', function(a){}, function(b){});
+        	
+
+        },
+        error: function (xhr) {
+          //  alert(xhr.responseText);
+            hideLoader();
+            window.plugins.toast.show(xhr.responseText, 'short', 'center', function(a){}, function(b){});
+        }
+    }).done(function () {
+        hideLoader();
+    });
+    }
+    else
+    {
+           window.plugins.toast.show('Internet not available !', 'short', 'center', function(a){}, function(b){});
+    }
+}
+
+
+function hideLoader() {
+
+	$('#loaderImage').css("display", "none");
+	$('.flex').css("display", "none");
+}
+
+function showLoader() {
+
+	$('#loaderImage').css("display", "block");
+	$('.flex').css("display", "block");
+	
+}
